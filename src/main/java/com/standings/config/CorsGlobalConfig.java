3d38d9ab22c0411c7546b9174d4cc2bcf6723872
Swarnim Shukla <swarnim0300@gmail.com>
2025-05-30 01:57:29 +0530

    add cross origin

---
+++ src/main/java/com/standings/config/CorsGlobalConfig.java
@@ -0,0 +1,26 @@
+package com.standings.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsGlobalConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173")); // frontend origin
+        config.setAllowedMethods(List.of("GET"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
