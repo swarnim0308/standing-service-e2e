935459f2741ead0f5becc10db7e9e2b7c74b7dd7
Swarnim Shukla <swarnim0300@gmail.com>
2025-05-30 01:31:26 +0530

    add cross origin

--- src/main/java/com/standings/controller/StandingsController.java
+++ src/main/java/com/standings/controller/StandingsController.java
@@ -4,11 +4,9 @@ import com.standings.service.StandingsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/v1")
 @RestController
 public class StandingsController {
